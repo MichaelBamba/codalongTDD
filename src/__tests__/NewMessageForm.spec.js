02dc8bed9e530cf03d46b371355f1ebab051d7dd
MichaelBamba <mjbamba2@gmail.com>
2020-04-09 12:03:57 -0400

    completed a codalong of a test based developement tutorial

---
+++ src/__tests__/NewMessageForm.spec.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import NewMessageForm from "../NewMessageForm";
+
+describe("<NewMessageForm />", () => {
+  let sendHandler;
+  let getByTestId;
+
+  afterEach(cleanup);
+
+  describe("clicking the send button", () => {
+    beforeEach(() => {
+      sendHandler = jest.fn();
+      ({ getByTestId } = render(<NewMessageForm onSend={sendHandler} />));
+
+      fireEvent.change(getByTestId("messageText"), {
+        target: {
+          value: "New message",
+        },
+      });
+
+      fireEvent.click(getByTestId("sendButton"));
+    });
+
+    it("clears the text field", () => {
+      expect(getByTestId("messageText").value).toEqual("");
+    });
+    it("calls the send handler", () => {
+      expect(sendHandler).toHaveBeenCalledWith("New message");
+    });
+  });
+});
